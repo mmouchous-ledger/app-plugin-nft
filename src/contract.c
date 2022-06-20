b30ee9d0a5b37853cbf471b8331081c7687df33c
Jean-Baptiste Bédrune <jean-baptiste.bedrune@ledger.fr>
2022-06-20 11:02:10 +0200

    Put contract info in a dedicated file

---
+++ src/contract.c
@@ -0,0 +1,14 @@
+#include "ledger_nft_plugin.h"
+
+// Function: mint
+// Selector: 0x1249c58b
+static const uint8_t MINT_SELECTOR[SELECTOR_SIZE] = {0x12, 0x49, 0xc5, 0x8b};
+// Function: preSaleMint
+// Selector: 0xc111fb91
+static const uint8_t PRE_SALE_MINT_SELECTOR[SELECTOR_SIZE] = {0xc1, 0x11, 0xfb, 0x91};
+
+// Array of all the different nft selectors.
+const uint8_t *const LEDGER_NFT_SELECTORS[NUM_SELECTORS] = {
+    MINT_SELECTOR,
+    PRE_SALE_MINT_SELECTOR,
+};
